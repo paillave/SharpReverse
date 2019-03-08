237edd6dc45754e613cd9f5339c0185543668105
Royer, Stephane <stephane.royer@kpmg.lu>
2019-03-08 15:48:45 +0100

    add task

---
+++ src/UmlReverseEngineeringTask.cs
@@ -0,0 +1,27 @@
+using System;
+using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+using Paillave.SharpReverse;
+
+namespace src
+{
+    public class UmlReverseEngineeringTask : Task
+    {
+        [Required]
+        public string AssemblyPath { get; set; }
+        public override bool Execute()
+        {
+            try
+            {
+                var reverseModel = new ReverseModel(this.AssemblyPath);
+                var outputPath = System.IO.Path.ChangeExtension(this.AssemblyPath, "yuml");
+                using (var sw = new System.IO.StreamWriter(outputPath))
+                    reverseModel.WriteYuml(sw);
+            }
+            catch (System.Exception)
+            {
+            }
+            return true;
+        }
+    }
+}
